743cf12d1c4b789b7c87ed2c8a0ecb1d418dbdf4
Richard Fenix <richard.fenix@gmail.com>
2018-03-26 23:29:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  else if int % 3 == 0
-    "Fizz"
-  else if int % 5 == 0
-    "Buzz"
+    return "FizzBuzz"
+  end
+  if int % 3 == 0
+    return "Fizz"
+  end
+  if int % 5 == 0
+    return "Buzz"
   end
 end
